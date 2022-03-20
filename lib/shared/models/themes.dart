2d4d3cdd958249fa4d8960f3fa940a540bfb54f7
TemiLayonn <65893987+temilayonn@users.noreply.github.com>
2022-03-20 07:02:41 +0100

    home screen
    added home screen down to checking products

---
+++ lib/shared/models/themes.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const Color white = Color(0xfffcfcff);
+const Color black = Colors.black;
+const Color primaryColor = Color(0xff7B61FF);
+const Color secondaryBlue = Color(0xff0C80E4);
+
+const Color primaryGrey = Color(0xff4F4F4F);
+const Color secondaryGrey = Color(0xff767676);
+//const Color ultraGrey = Color(0xffF3F3F3);
+
+//add your styles here
+// ignore: prefer_const_constructors
+// TextStyle titleTextStyle = TextStyle(
+//     fontSize: 20,
+//     color: black,
+//     fontStyle: FontStyle.normal,
+//     fontWeight: FontWeight.w500,
+//     fontFamily: 'PT Sans');
+
+// // ignore: prefer_const_constructors
+// TextStyle subtitleTextStyleGrey = TextStyle(
+//     fontSize: 14,
+//     color: Color(0xff212120),
+//     fontStyle: FontStyle.normal,
+//     fontWeight: FontWeight.w300,
+//     fontFamily: 'PT Sans');
+
+// TextStyle subtitleTextStyleBoldGrey = TextStyle(
+//     fontSize: 14,
+//     color: Color(0xff767676),
+//     fontStyle: FontStyle.normal,
+//     fontWeight: FontWeight.w600,
+//     fontFamily: 'PT Sans');
+
+// TextStyle listingSummaryStyle = TextStyle(
+//     fontWeight: FontWeight.w600,
+//     fontSize: 14,
+//     color: black,
+//     fontFamily: 'PT Sans');
